eedcf868ecc192a9f7cb6762f19ab8cb3ce259b4
samanafsha <116269536+samanafsha@users.noreply.github.com>
2022-10-20 16:10:13 +0530

    Update streamlit_app.py
    added new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My new healthy diner')
-streamlit.titlie('Breakfast Menu')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omeg 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlt.text('Hard-Bolied Free-Range Egg')
