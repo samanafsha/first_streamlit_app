e4f357cdb8cd350dc9d90c09cbc0094fd5795a0a
samanafsha <116269536+samanafsha@users.noreply.github.com>
2022-10-20 18:04:56 +0530

    Update streamlit_app.py
    fix fruits names list with index

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('Hard-Bolied Free-Range Egg')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 
@@ -20,4 +21,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page
 
 streamlit.dataframe(my_fruit_list)
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
