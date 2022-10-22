3605d47b7a431e60a2554860cb7d3e635dc9dde3
samanafsha <116269536+samanafsha@users.noreply.github.com>
2022-10-22 19:57:58 +0530

    Update streamlit_app.py
    edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,12 +33,6 @@ streamlit.dataframe(fruits_to_show)
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # #streamlit.text(fruityvice_response.json())
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 # # take the json version of the response ans normalise it
 # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -56,3 +50,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
