e7d0ba34420d4d61c44e9d9cd605becbe78484a1
samanafsha <116269536+samanafsha@users.noreply.github.com>
2022-10-22 23:23:16 +0530

    Update streamlit_app.py
    edits formating

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My new healthy diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -10,46 +13,41 @@ streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Bolied Free-Range Egg')
 
 
-import pandas
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected] 
-# Display the table on the page
 
-# streamlit.dataframe(my_fruit_list)
+# Display the table on the page
+streamlit.dataframe(fruits_to_show)
+
 # #my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.dataframe(fruits_to_show)
 
 #-------------------------------------------------------------------
 
-# streamlit.header("Fruityvice Fruit Advice!")
-# import requests
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# #streamlit.text(fruityvice_response.json())
-
-
-# # take the json version of the response ans normalise it
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# # output in the screen as table
-# streamlit.dataframe(fruityvice_normalized)
-
-# # new section to display fruityvice api response
+# # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
+
+
+#dont run anything past here hwile we trubleshoot
+streamlit.stop()
+
+
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
