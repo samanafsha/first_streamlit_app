d2fab36abef52d70eb405c345e13f558d40e5139
samanafsha <116269536+samanafsha@users.noreply.github.com>
2022-10-22 21:25:59 +0530

    Update streamlit_app.py
    list added

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
